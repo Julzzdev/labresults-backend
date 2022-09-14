b7144596964381ae5a168c5a22397ce7301d569a
Julio Olvera <julioolveraunt@gmail.com>
2022-09-14 11:24:29 -0500

    fix on mail

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -19,7 +19,6 @@ export class MailerService {
     const mailUser = await this.mailerModel.findOne();
 
     const browser = await puppeteer.launch({
-      executablePath: '/usr/bin/chromium-browser',
       headless: true,
       args: [
         '--disable-gpu',
