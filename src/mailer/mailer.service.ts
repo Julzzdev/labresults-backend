eef2ca51226500ad70e75c980bbf595c2d235dfe
Julio Olvera <julioolveraunt@gmail.com>
2022-09-14 11:04:44 -0500

    soy un imbecil

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -32,7 +32,7 @@ export class MailerService {
     });
     const page = await browser.newPage();
     await page.emulateTimezone('America/Mexico_City');
-    const baseUrl = 'http://localhost:80/reports/';
+    const baseUrl = 'http://front:80/reports/';
     await page.goto(`${baseUrl}${patientId}/${isFlat}`, {
       waitUntil: 'networkidle0',
     });
