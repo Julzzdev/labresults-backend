fffbf4396b4fcffd4bfa922d87a140cf1a4d3e93
Julio Olvera <julioolveraunt@gmail.com>
2022-09-14 12:13:33 -0500

    Update mailer.service.ts

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -19,7 +19,7 @@ export class MailerService {
     const mailUser = await this.mailerModel.findOne();
 
     const browser = await puppeteer.launch({
-      executablePath: '/usr/bin/chromium-browser',
+      // executablePath: '/usr/bin/chromium-browser',
       headless: true,
       args: [
         '--disable-gpu',
@@ -33,7 +33,7 @@ export class MailerService {
     const page = await browser.newPage();
     await page.emulateTimezone('America/Mexico_City');
     const baseUrl = 'http://front:80/reports/';
-    await page.goto(`${baseUrl}${patientId}/${isFlat}`, {
+    await page.goto(baseUrl + patientId + '/' + isFlat, {
       waitUntil: 'networkidle0',
     });
     const pdf = await page.pdf({ format: 'A4' });
